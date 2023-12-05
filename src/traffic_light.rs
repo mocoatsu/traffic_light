d9279fae4761db2996087c9184f862241ebdb6bf
mocoatsu <mocomocoaimai673@gmail.com>
2023-12-05 21:07:12 +0900

    fixup! feature: changing traffic_light

--- src/traffic_light.rs
+++ src/traffic_light.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub enum TrafficLightState {
     Red,
     Yellow,
@@ -20,5 +20,25 @@ impl TrafficLightState {
             TrafficLightState::Yellow => 1,
             TrafficLightState::Green => 5,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_traffic_light_state_transition() {
+        let state = TrafficLightState::Red;
+        assert_eq!(state.next(), TrafficLightState::Green);
+        assert_eq!(state.next().next(), TrafficLightState::Yellow);
+        assert_eq!(state.next().next().next(), TrafficLightState::Red);
+    }
+
+    #[test]
+    fn test_traffic_light_state_duration() {
+        assert_eq!(TrafficLightState::Red.duration(), 6);
+        assert_eq!(TrafficLightState::Yellow.duration(), 1);
+        assert_eq!(TrafficLightState::Green.duration(), 5);
     }
 }
